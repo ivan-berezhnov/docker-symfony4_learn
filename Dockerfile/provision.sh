a076da50ae190b98185bd8c45f2687510cd19ce0
Ivan Berezhnov <domainsua@gmail.com>
2018-01-08 21:00:16 +0200

    Init project

---
+++ Dockerfile/provision.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+curl -sS https://getcomposer.org/installer | php -- --filename=composer --install-dir=/usr/bin
+
+cd /var/www/
+mkdir -p /usr/local/bin
+curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
+chmod a+x /usr/local/bin/symfony
+mkdir web
+#symfony new web 3.4.2
+a2ensite 000-default.conf
+service apache2 restart
